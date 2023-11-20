7f4fc2b9e5da12e30fcf8a3d02397c4e7a4c630c
ProudScottsTot <87554004+ProudScottsTot@users.noreply.github.com>
2023-11-20 14:09:59 -0500

    Update visual.py

--- visual.py
+++ visual.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import pyvis as pv
 import numpy as np
 from collections import Counter
 
@@ -23,6 +22,8 @@ df['Instagram Follower #'] = df['Instagram Follower #'] .fillna(0)
 df['Threads Follower #'] = df['Threads Follower #'].fillna(0)
 df['YouTube Subscriber #']  = df['YouTube Subscriber #'] .fillna(0)
 df['TikTok Subscriber #'] = df['TikTok Subscriber #'].fillna(0)
+
+df.set_index('Name (English)', inplace=True)
 
 #total following for each account
 df['Total Prescence'] = df['X (Twitter) Follower #'] + df['Facebook Follower #'] + df['Instagram Follower #'] + df['Threads Follower #'] + df['YouTube Subscriber #'] + df['TikTok Subscriber #']
@@ -64,50 +65,83 @@ res = dict(sorted(Counter(entity_owner.values()).items(), key=lambda item: item[
 
 Y1 =entity_count
 
-X2 = res.keys()
-Y2 = res.values()
+X2 = list(res.keys())
+Y2 = list(res.values())
 
 Y3 = region_count
 
-X4 = df['Name (English)']
+X4 = df['Total Prescence'].index.tolist()
 Y4 = df['Total Prescence']
 
 Y5 = df['Impact Metric']
 
 Y6 = df['Impact Metric'] - df['Total Prescence']
 
-print(Y6)
 
-figure, axis = plt.subplots(2, 3)
+plt.bar(Y1.keys(),Y1)
+plt.title("No. of Accounts of each Entity Owners") 
+plt.yscale('log')
+plt.ylabel('log no. of accounts')
+plt.xlabel('entity owners')
+plt.gca().get_xaxis().set_visible(False)
+#plt.show()
+
+plt.bar(X2,Y2)
+plt.title("No. of Entity Owners of each Parent") 
+plt.xlabel('parent entity')
+plt.ylabel('no. of entity owners')
+plt.gca().get_xaxis().set_visible(False)
+#plt.show()
+
+plt.bar(Y3.keys(),Y3)
+plt.title("Region of Focus Account Number") 
+plt.xlabel('no. region of focus')
+plt.ylabel('log no. of accounts')
+plt.gca().get_xaxis().set_visible(False)
+plt.yscale('log')
+#plt.show()
+
+plt.bar(X4,Y4)
+plt.title("Total Following") 
+plt.gca().get_xaxis().set_visible(False)
+plt.xlabel('account')
+plt.ylabel('log total following')
+plt.yscale('log')
+#plt.show()
+
+plt.bar(X4,Y5)
+plt.title("Impact Rating") 
+plt.xlabel('account')
+plt.ylabel('log impact rating')
+plt.gca().get_xaxis().set_visible(False)
+plt.yscale('log')
+#plt.show()
+
+plt.bar(X4,Y6)
+plt.title("Difference Between Metric and Total") 
+plt.xlabel('account')
+plt.ylabel('difference')
+plt.gca().get_xaxis().set_visible(False)
+#plt.show()
+
+def write_report(Y):
+    print(f'The mean of this set is {np.mean(Y)}')
+    print(f'The quartiles are: Q1 = {np.quantile(Y,0.25)}, Q2 = {np.quantile(Y,0.5)}, Q3 = {np.quantile(Y,0.75)}')
 
 
-axis[0, 0].bar(Y1.keys(),Y1)
-axis[0, 0].set_title("Entity Owner Count") 
-axis[0,0].set_yscale('log')
-axis[0,0].get_xaxis().set_visible(False)
-
-axis[0, 1].bar(X2,Y2)
-axis[0, 1].set_title("Parent Entity Count") 
-axis[0, 1].get_xaxis().set_visible(False)
-
-axis[0,2].bar(Y3.keys(),Y3)
-axis[0,2].set_title("Region of Focus Count") 
-axis[0,2].get_xaxis().set_visible(False)
-axis[0,2].set_yscale('log')
-
-axis[1,0].bar(X4,Y4)
-axis[1,0].set_title("Total Prescence Count") 
-axis[1,0].get_xaxis().set_visible(False)
-axis[1,0].set_yscale('log')
-
-axis[1,1].bar(X4,Y5)
-axis[1,1].set_title("Impact Metric") 
-axis[1,1].get_xaxis().set_visible(False)
-axis[1,1].set_yscale('log')
-
-axis[1,2].bar(X4,Y6)
-axis[1,2].set_title("Difference Between Metric and Total") 
-axis[1,2].get_xaxis().set_visible(False)
-plt.show()
+series = [Y1,Y2,Y3,Y4,Y5,Y6]
+for i in range(6):
+    if i == 0:
+        write_report(series[i])
+        print(f'The top entity owners are {series[i].head(10)}')
+    if i == 1:
+        write_report(series[i])
+        print(f'The top parent entities are {X2[0:10]}')
+    if i == 2:
+        write_report(series[i])
+        print(f'The top regions of focus are {series[i].head(10)}')
+    elif i == 3 or i == 4:
+        write_report(series[i])
+        print(f'The top accounts are {series[i].head(10)}')
 
 
